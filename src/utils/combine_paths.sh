109a0239870e944b8c69d569b6a8c3a5c408a0ed
gventurini <intesasanpaolo>
2020-11-21 20:11:37 +0100

    remove bloat code

--- src/utils/combine_paths.sh
+++ src/utils/combine_paths.sh
@@ -1,15 +1,21 @@
-# combines two paths: P1, P2 => P1/P2
-function combine_paths {
-    if [[ "$1" == */ ]] && [[ "$2" == /* ]]
-    then
-        combine_result="$1${2:1}"
-    elif [[ "$1" == */ ]] || [[ "$2" == /* ]]
-    then
-        combine_result="$1$2"
-    else
-        combine_result="$1/$2"
-    fi
-    echo $combine_result
-}
+#!/bin/bash
 
-combine_paths "$1" "$2"
+# combines two paths: P1, P2 => P1/P2
+
+if [ $# -ne 2 ]
+then
+    echo "Wrong arguments number, expected 2 got $#"
+    exit -1
+fi
+
+if [[ "$1" == */ ]] && [[ "$2" == /* ]]
+then
+    combine_result="$1${2:1}"
+elif [[ "$1" == */ ]] || [[ "$2" == /* ]]
+then
+    combine_result="$1$2"
+else
+    combine_result="$1/$2"
+fi
+
+echo $combine_result
